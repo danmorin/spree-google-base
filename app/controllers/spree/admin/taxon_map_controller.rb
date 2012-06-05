739ef255a401495e0175dda0ceec20b3f6f69bb1
Dan Morin <dan.morin@gmail.com>
2012-06-05 16:01:57 -0400

    Changed for the controller receives and saves taxon maps

--- app/controllers/spree/admin/taxon_map_controller.rb
+++ app/controllers/spree/admin/taxon_map_controller.rb
@@ -1,4 +1,4 @@
-class Spree::Admin::TaxonMapController < Spree::Admin::ResourceController
+class Spree::Admin::TaxonMapController < Spree::Admin::BaseController
 
   def index
     @taxons = Spree::Taxon.includes(:taxon_map).order(:id).all
@@ -12,14 +12,14 @@ class Spree::Admin::TaxonMapController < Spree::Admin::ResourceController
   end
 
   def create
-    Spree::TaxonMap.delete(Spree::TaxonMap.find(:all))
-    params[:tax_id].each do |k, v|
-      taxon_map = Spree::TaxonMap.new(:product_type => v, :taxon_id => k, :priority => params[:priority][k].to_i || 0)
-      taxon_map.save
+    taxon_maps = Spree::TaxonMap.all
+    params[:taxon_map].each do |id, values|
+      id = id.to_i
+      taxon_map = taxon_maps.detect { |tm| tm.id == id }
+      next unless taxon_map.present?
+      taxon_map.update_attributes(values)
     end
-    if Spree::TaxonMap.count == params[:tax_id].size
-      flash[:notice] = "Google Base taxons mapping saved successfully."
-    end
+    flash[:notice] = "Google Base taxons mapping saved successfully."
     redirect_to admin_taxon_map_index_url
   end
 end
